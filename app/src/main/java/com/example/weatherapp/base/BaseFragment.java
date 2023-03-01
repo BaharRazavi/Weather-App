94bc374a8ff3a1e70a4bcc1af87af922970457c5
Bahar Razavi <s.bahar.razavi@gmail.com>
2023-03-01 18:15:04 +0330

    creating the project

---
+++ app/src/main/java/com/example/weatherapp/base/BaseFragment.java
@@ -0,0 +1,6 @@
+package com.example.weatherapp.base;
+
+import androidx.fragment.app.Fragment;
+
+public class BaseFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/weatherapp/ui/fragment/DetailsFragment.java
@@ -0,0 +1,26 @@
+package com.example.weatherapp.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.weatherapp.R;
+import com.example.weatherapp.base.BaseFragment;
+import com.example.weatherapp.databinding.FragmentDetailsBinding;
+
+public class DetailsFragment extends BaseFragment {
+
+    private FragmentDetailsBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailsBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+}
